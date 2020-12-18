69a38a56307e73a35d44b97e573e685b514db159
abhinavjnvb <abhinavjnvb@gmail.com>
2020-12-19 00:37:37 +0530

    integrate express-validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,22 @@
 const express =  require('express');
 const router = express.Router();
+const {body, validationResult} = require('express-validator');
 
-
-// @route GET api/users
+// @route POST api/users
 // @desc  Test route
 // @access Public
 
-router.get('/',(req,res)=>res.send("User Route."));
+router.post('/',[
+    body("name","Name is required.").not().isEmpty(),
+    body("email","Please enter a valid email.").isEmail(),
+    body("password","Please enter a password with 6 or more characters.").isLength({min:6})
+]
+    ,(req,res)=>{
+     const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+    res.send("User Route.")
+});
 
 module.exports = router;
